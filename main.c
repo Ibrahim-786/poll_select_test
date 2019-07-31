94423a3351f83bf36d139390841a1c35c73dca1c
Ricardo Biehl Pasquali <pasqualirb@gmail.com>
2019-07-31 16:18:58 -0300

    Make single thread default
    Polling is now in main thread.
    
    Signed-off-by: Ricardo Biehl Pasquali <pasqualirb@gmail.com>

--- main.c
+++ main.c
@@ -18,28 +18,19 @@
 // open_socket() do_send() do_poll() do_recv()
 #include "common.h"
 
-static int send_in_same_thread = 0;
+static int send_in_other_thread = 0;
 static int use_select = 0;
 static short request_mask = 0;
 
-// It's a thread. It does poll/recv, or send/poll/recv (when
-// SEND_IN_SAME_THREAD is defined)
+// Sender thread used when send_in_other_thread is true
 static void*
-loop(void *data)
+sender(void *data)
 {
 	int sfd = *((int*) data);
 
 	for (;;) {
-		if (send_in_same_thread) {
-			sleep(1);
-			do_send(sfd);
-		}
-
-		if (use_select)
-			do_select(sfd, request_mask);
-		else
-			do_poll(sfd, request_mask);
-		do_recv(sfd);
+		sleep(1);
+		do_send(sfd);
 	}
 
 	return NULL;
@@ -50,8 +41,7 @@ const char help_text[] =
 "--pollin   Request POLLIN event.\n"
 "--pollerr  Request POLLERR event (ignored because this\n"
 "           event is always requested).\n"
-"--single-thread  Send packets in the same thread that\n"
-"                 poll for their timestamps.\n"
+"--multi-thread   Send packets in another thread.\n"
 "--use-select     Use select() system call instead of\n"
 "                 poll().\n"
 "--mask-pollpri   Mask POLLPRI in wake up. This set\n"
@@ -65,7 +55,7 @@ const char help_text[] =
 int
 main(int argc, char **argv)
 {
-	pthread_t loop_thread;
+	pthread_t sender_thread;
 	int sfd;
 	int flags = 0;
 
@@ -76,7 +66,7 @@ main(int argc, char **argv)
 			{ "pollpri",       no_argument, 0, 'p' },
 			{ "pollin",        no_argument, 0, 'i' },
 			{ "pollerr",       no_argument, 0, 'e' },
-			{ "single-thread", no_argument, 0, 's' },
+			{ "multi-thread",  no_argument, 0, 's' },
 			{ "use-select",    no_argument, 0, 'S' },
 			{ "mask-pollpri",  no_argument, 0, 'm' },
 			{ "bind-socket",   no_argument, 0, 'b' },
@@ -96,7 +86,7 @@ main(int argc, char **argv)
 		case 'p': request_mask |= POLLPRI; break;
 		case 'i': request_mask |= POLLIN;  break;
 		case 'e': request_mask |= POLLERR; break;
-		case 's': send_in_same_thread = 1; break;
+		case 's': send_in_other_thread = 1; break;
 		case 'S': use_select = 1;          break;
 		case 'm': flags |= POLLPRI_WAKEUP_ON_ERROR_QUEUE; break;
 		case 'b': flags |= BIND_SOCKET;                   break;
@@ -108,15 +98,24 @@ main(int argc, char **argv)
 		return 1;
 
 	// create thread and wait for it to terminate
-	pthread_create(&loop_thread, NULL, loop, &sfd);
-	if (!send_in_same_thread) {
-		for (;;) {
+	if (send_in_other_thread)
+		pthread_create(&sender_thread, NULL, sender, &sfd);
+
+	for (;;) {
+		if (!send_in_other_thread) {
 			sleep(1);
 			do_send(sfd);
 		}
+
+		if (use_select)
+			do_select(sfd, request_mask);
+		else
+			do_poll(sfd, request_mask);
+		do_recv(sfd);
 	}
 
-	pthread_join(loop_thread, NULL);
+	if (send_in_other_thread)
+		pthread_join(sender_thread, NULL);
 
 	return 0;
 }
